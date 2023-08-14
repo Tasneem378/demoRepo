e131e45c113cd848a22a4e6c2499fde863c1987d
Tasneem378 <tasneem378@gmail.com>
2023-08-14 13:29:45 +0500

    code add

--- Question.js
+++ Question.js
@@ -4,3 +4,6 @@ localStorage.setItem('Location', 'Islamabad');
 console.log(localStorage.getItem('Location'));
 console.log(localStorage.getItem('FirstName'));
 
+const person = ['Tasneem','Fakhara','Aysha','Hassan','Hussain'];
+console.log(person);
+
