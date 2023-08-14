c2dce34a306662097cb848ff68dd01ca5af8d4d7
Tasneem378 <tasneem378@gmail.com>
2023-08-14 11:50:10 +0500

    Update Question.js

--- Question.js
+++ Question.js
@@ -2,4 +2,5 @@ localStorage.setItem('FirstName', 'Tasneem');
 localStorage.setItem('Location', 'Islamabad');
 
 console.log(localStorage.getItem('Location'));
+console.log(localStorage.getItem('FirstName'));
 
